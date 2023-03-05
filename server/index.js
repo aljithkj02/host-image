3127852c1731b45659037bdd5103ccb6e4af0031
Aljith KJ <kwdeathlok@gmail.com>
2023-03-05 15:30:22 +0530

    DB added and connected

--- server/index.js
+++ server/index.js
@@ -3,13 +3,16 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const connectDB = require('./config/db');
+
 const app = express();
 app.use(cors());
 app.use(express.json())
 
 
 const serverStart = () => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT, async () => {
+        await connectDB();
         console.log(`Server is running on port ${process.env.PORT}`)
     })
 }
