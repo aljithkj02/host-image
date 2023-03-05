4303795962d2d348664f9a35e0fcf970f6e2c668
Aljith KJ <kwdeathlok@gmail.com>
2023-03-05 11:35:57 +0530

    Login page UI created

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Box, Text, Input, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Login = () => { 
   return (
-    <div>
-      Login
-    </div>
+    <Box display="flex" justifyContent="center" alignItems="center" h="80vh">
+      <Box w={["95%", "90%", "70%", "50%"]} boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" p="70px 50px" borderRadius="lg"
+        display="flex" flexDir="column" gap="20px"
+      >
+        <Text fontSize="3xl" textAlign="center">Login</Text>
+        <form style={{ display: 'flex', flexDirection: 'column', gap: '20px'}}>
+          <Input type="email" placeholder="Email" variant="flushed" />
+          <Input type="password" placeholder="Password" variant="flushed" />
+          <Button mt={6} type="submit" colorScheme='messenger'
+            p={5} fontSize="16px"
+          >Login</Button>
+          <Text color="gray" fontSize="sm" textAlign="center"
+          >Don't have an account? 
+            <Link to="/signup" style={{ marginLeft: '5px', color: 'blue', fontWeight: '500' }}
+            >Signup</Link>
+          </Text>
+        </form>
+      </Box>
+    </Box>
   )
 }
 
--- client/src/routes/AllRoutes.tsx
+++ client/src/routes/AllRoutes.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { Container } from '@chakra-ui/react'
 import { Home, Login, Signup, Upload } from '../pages'
 
 const AllRoutes = () => {
   return (
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-    </Routes>
+      <Container maxW={['95%', '90%', '85%']} py={5}>
+        <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/upload" element={<Upload />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Container>
   )
 }
 
