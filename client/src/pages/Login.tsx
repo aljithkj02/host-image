4fcb179e7ba3573dd8dcae2bb4f8443832eeb794
Aljith KJ <kwdeathlok@gmail.com>
2023-03-05 22:06:13 +0530

    Signup, login data storing part completed

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Text, Input, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const Login = () => { 
+  const [details, setDetails] = useState({
+      email: '',
+      password: ''
+  })
+  const handleChange = (e) => {
+      setDetails({
+        ...details,
+        [e.target.name]: e.target.value
+      })
+  }
+
+  const loginUser = (e) => {
+    e.preventDefault()
+    console.log(details)
+  }
+  
   return (
     <Box display="flex" justifyContent="center" alignItems="center" h="80vh">
       <Box w={["95%", "90%", "70%", "50%"]} boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" p="70px 50px" 
         borderRadius="lg" display="flex" flexDir="column" gap="20px"
       >
         <Text fontSize="3xl" textAlign="center">Login</Text>
-        <form style={{ display: 'flex', flexDirection: 'column', gap: '20px'}}>
-          <Input type="email" placeholder="Email" variant="flushed" />
-          <Input type="password" placeholder="Password" variant="flushed" />
+        <form style={{ display: 'flex', flexDirection: 'column', gap: '20px'}}
+          onSubmit={ loginUser }
+        >
+          <Input type="email" name="email" placeholder="Email" variant="flushed" onChange={ handleChange }
+            required="required"
+          />
+          <Input type="password" name="password" placeholder="Password" variant="flushed" onChange={ handleChange }
+            required="required"
+          />
           <Button mt={6} type="submit" colorScheme='messenger'
             p={5} fontSize="16px"
           >Login</Button>
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Text, Input, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import config from '../config';
 
 const Signup = () => {
+  const [details, setDetails] = useState({
+      name: '',
+      email: '',
+      password: ''
+  })
+  const handleChange = (e) => {
+      setDetails({
+        ...details,
+        [e.target.name]: e.target.value
+      })
+  }
+
+  const signupUser = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await axios.post(`${config.API_URL}/api/user/signup`, { ...details });
+      console.log(res?.data?.message);
+      if(res?.data?.status){
+          const token = res?.data?.token;
+      }
+    } catch (err) {
+      console.log(err?.response?.data?.message);
+    }
+  }
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center" h="80vh">
       <Box w={["95%", "90%", "70%", "50%"]} boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" p="70px 50px" borderRadius="lg"
         display="flex" flexDir="column" gap="20px"
       >
         <Text fontSize="3xl" textAlign="center">Signup</Text> 
-        <form style={{ display: 'flex', flexDirection: 'column', gap: '20px'}}>
-          <Input type="name" placeholder="Name" variant="flushed" />
-          <Input type="email" placeholder="Email" variant="flushed" />
-          <Input type="password" placeholder="Password" variant="flushed" />
+        <form style={{ display: 'flex', flexDirection: 'column', gap: '20px'}}
+          onSubmit={ signupUser }
+        >
+          <Input type="name" name="name" placeholder="Name" variant="flushed" onChange={ handleChange }
+            required="required"
+          />
+          <Input type="email" name="email" placeholder="Email" variant="flushed" onChange={ handleChange }
+            required="required"
+          />
+          <Input type="password" name="password" placeholder="Password" variant="flushed" onChange={ handleChange }
+            required="required"
+          />
           <Button mt={6} type="submit" colorScheme='messenger'
             p={5} fontSize="16px"
           >Signup</Button>
