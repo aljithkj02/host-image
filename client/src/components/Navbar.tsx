990703eb58b0019debe1d5ad510719b5c9e9842c
Aljith KJ <kwdeathlok@gmail.com>
2023-03-06 22:21:34 +0530

    logout confirm modal added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import { Box, Text, Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Box, Text, Button, useToast } from '@chakra-ui/react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useData, useAction } from '../hooks';
+import Confirm  from './Confirm';
 
 const Navbar = () => {
+  const [ isOpen, setIsOpen] = useState(false);
   const { isAuth, name } = useData();
   const { logout, dispatch } = useAction();
   const navigate = useNavigate();
+  const toast = useToast()
+  
   const logoutUser = () => {
+    toast({
+        title: 'User logout successfully!',
+        status: 'success',
+        position: 'top',
+        isClosable: true,
+    })
+    handleOpen();
     dispatch(logout());
   }
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <nav>
+      { isOpen && <Confirm handlerNo={handleOpen} handlerYes={ logoutUser } 
+        message="Do you want to logout?" 
+      />}
       <Box display="flex"
         justifyContent="space-between"
         alignItems="center"
@@ -29,7 +48,7 @@ const Navbar = () => {
             {
               isAuth? (
                 <Button size="sm" colorScheme="red"
-                  onClick= { logoutUser }
+                  onClick= { handleOpen }
                 >Logout</Button>
               ) : (
                 <NavLink to="/login">Login</NavLink>
