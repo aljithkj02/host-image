2ef04bc0830e07b2ca8078c2726e4e5f8544abc0
Aljith KJ <kwdeathlok@gmail.com>
2023-03-06 14:58:03 +0530

    image upload functionality added

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Text, Input, Button } from '@chakra-ui/react';
+import axios from 'axios';
+import config from '../config';
+import { useAction } from '../hooks';
 
 const Upload = () => {
+  const [ image, setImage] = useState(null);
+  const { loadingOn, loadingOff, dispatch } = useAction();
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append('image', image);
+      const token = localStorage.getItem('token') || 'token';
+      console.log(token);
+      const res = await axios.post(`${config.API_URL}/api/gallery/upload`, formData, {
+        headers: {
+          'authorization': `Bearer ${token}`
+        }
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err?.response.data.message);
+    }
+  }
   return (
     <Box display="flex" justifyContent="center" alignItems='center' h="80vh">
       <Box display="flex" flexDir="column" gap={10} borderRadius="lg"
         w={["95%", "90%", "70%", "50%"]} boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" p="70px 50px" 
       >
         <Text fontSize="3xl" textAlign="center">Upload image</Text>
-        <Box display={"flex"} gap={5} flexDir={["column", "column", "row"]}>
-          <Input type="file" />
-          <Button colorScheme='teal' variant='solid' px="20px">Upload</Button>
-        </Box>
+          <form onSubmit={ handleSubmit }>
+            <Box display={"flex"} gap={5} flexDir={["column", "column", "row"]} 
+              alignItems={["none", "none", "center"]} justifyContent="space-between"
+            >
+                <input type="file" name="image" onChange={ handleImageChange }/>
+                <Button type='submit' colorScheme='teal' variant='solid' px="20px">Upload</Button>
+            </Box>
+          </form>
       </Box>
     </Box>
   )
