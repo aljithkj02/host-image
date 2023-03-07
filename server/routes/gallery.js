dd1d841dbfff8d119460a9d8a799e45163ebe57d
Aljith KJ <kwdeathlok@gmail.com>
2023-03-07 11:54:52 +0530

    Image storing method changed, get-signature route added

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const multer = require('multer');
-const { uploadImage, getAllImages, getThumbnails, deleteImage } = require('../controllers/gallery.controller'); 
+const { uploadImage, getAllImages, getThumbnails, deleteImage, getSignature } = require('../controllers/gallery.controller'); 
 const authorize = require('../middlewares/authorization');
 
 const upload = multer({ dest: './uploads/' });
@@ -9,7 +9,8 @@ const router = Router();
 router.get('/', authorize, getAllImages);
 router.delete('/delete-image/:id', authorize, deleteImage);
 router.get('/thumbnails/:id', authorize, getThumbnails);
-router.post('/upload', upload.single('image'), authorize, uploadImage );
+router.post('/upload', authorize, uploadImage );
+router.get('/get-signature', authorize, getSignature);
 
 
  
